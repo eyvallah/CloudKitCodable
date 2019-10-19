268785a1cfe8ef0b338ab950e87a0ed26b380772
lilinfeng <lilinfeng303@outlook.com>
2019-10-19 18:45:09 +0800

    swift 5.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["CloudKitCodable"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/eyvallah/OpenCloudKit.git", from: "0.6.7"),
+        .package(url: "https://github.com/eyvallah/OpenCloudKit.git", from: "0.6.8"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.

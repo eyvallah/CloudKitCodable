2f94344c046b87e90aad273ad9b6316c79e0546b
Levin Li <lilinfeng303@outlook.com>
2020-10-10 17:54:08 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["CloudKitCodable"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/eyvallah/OpenCloudKit.git", from: "0.6.8"),
+        .package(url: "https://github.com/eyvallah/OpenCloudKit.git", from: "0.7.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
